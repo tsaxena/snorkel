181393a312b1b0d86155ab25996f256c7c9b7b83
Alexander Ratner <ajratner@gmail.com>
2017-02-12 20:30:55 -0800

    Working on CDR tutorial

--- tutorials/cdr/transfer_tables.sh
+++ tutorials/cdr/transfer_tables.sh
@@ -7,13 +7,14 @@
 # 3. Drop the CDR corpus and insert the biocorpus data.
 #
 # Then, you can re-run the CDR CorpusParser to add back in the CDR corpus properly.
+echo "Transferring from $1 into $2..."
 
 # Transfer tables from snorkel-biocorpus-new DB
-pg_dump -t context_filtered_kw snorkel-biocorpus-new | psql cdr-structure-learning-2
-pg_dump -t document_filtered_kw snorkel-biocorpus-new | psql cdr-structure-learning-2
-pg_dump -t sentence_filtered_kw snorkel-biocorpus-new | psql cdr-structure-learning-2
+pg_dump -t context_filtered_kw $1 | psql $2
+pg_dump -t document_filtered_kw $1 | psql $2
+pg_dump -t sentence_filtered_kw $1 | psql $2
 
 # Delete overlap with tables
-psql -d cdr-structure-learning-2 -c "DELETE FROM context_filtered_kw cf WHERE cf.stable_id IN (SELECT stable_id FROM context);"
-psql -d cdr-structure-learning-2 -c "DELETE FROM sentence_filtered_kw sf USING document_filtered_kw df WHERE sf.document_id = df.id AND df.name IN (SELECT name FROM document);"
-psql -d cdr-structure-learning-2 -c "DELETE FROM document_filtered_kw df WHERE df.name IN (SELECT name FROM document);"
+psql -d $2 -c "DELETE FROM context_filtered_kw cf WHERE cf.stable_id IN (SELECT stable_id FROM context);"
+psql -d $2 -c "DELETE FROM sentence_filtered_kw sf USING document_filtered_kw df WHERE sf.document_id = df.id AND df.name IN (SELECT name FROM document);"
+psql -d $2 -c "DELETE FROM document_filtered_kw df WHERE df.name IN (SELECT name FROM document);"
